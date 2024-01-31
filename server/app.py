2f3ffd2b96e2d53b669aa71d42d6d9f69974ee26
hesbonmanyi254 <hesbonmanyinsa96@gmail.com>
2024-01-31 14:58:46 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
+# server/app.py
 
 from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+@app.route('/')
+def index():
+    return 'Hello, World!'
